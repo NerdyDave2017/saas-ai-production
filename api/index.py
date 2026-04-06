51cbb2cb9d448cbb84083321098678e140252806
NerdyDave2017 <nerdydave2017@gmail.com>
2026-04-06 14:00:07 +0100

    feat: add streaming and improve styling

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI
-from fastapi.responses import PlainTextResponse
-from openai import OpenAI
+from fastapi import FastAPI  # type: ignore
+from fastapi.responses import StreamingResponse  # type: ignore
+from openai import OpenAI  # type: ignore
 from dotenv import load_dotenv
 import os
 
@@ -11,15 +11,25 @@ load_dotenv(override=True)
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 OPENROUTER_BASE_URL = os.getenv("OPENROUTER_BASE_URL")
 
-@app.get("/api", response_class=PlainTextResponse)
+@app.get("/api")
 def idea():
   client = OpenAI(
     api_key=OPENROUTER_API_KEY,
     base_url=OPENROUTER_BASE_URL
   )
-  prompt = [{"role": "user", "content": "Come up with a new business idea for AI Agents"}]
-  response = client.chat.completions.create(model="gpt-5-nano", messages=prompt)
-  return response.choices[0].message.content
+  prompt = prompt = [{"role": "user", "content": "Reply with a new business idea for AI Agents, formatted with headings, sub-headings and bullet points"}]
+  stream = client.chat.completions.create(model="gpt-5-nano", messages=prompt, stream=True)
+
+  def event_stream():
+    for chunk in stream:
+      text = chunk.choices[0].delta.content
+      if text:
+          lines = text.split("\n")
+          for line in lines:
+              yield f"data: {line}\n"
+          yield "\n"
+
+  return StreamingResponse(event_stream(), media_type="text/event-stream")
 
 
   
